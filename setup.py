07cf8670f5b635821a5b949e7eda1a4065b4e6e1
윤기현 <gih2yun@baikal.ai>
2025-06-05 15:12:55 +0900

    의존성을 모두 개선하여 반영
    grpcio 1.53.2로 변경
    protobuf 3.20.3으로 최신화 처리
    bareun-apis 0.15.0으로 최신화
    빌드환경 vscode에 맞게 구성

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
 Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
+Programming Language :: Python :: 3.13
 Programming Language :: Python :: 3 :: Only
 Natural Language :: Korean
 Development Status :: 5 - Production/Stable
@@ -39,7 +43,7 @@ Operating System :: MacOS
 
 setuptools.setup(
     name="bareunpy",
-    version="1.6.5",
+    version="1.7.0",
     author="Gihyun YUN",
     author_email="gih2yun@baikal.ai",
     description="The bareun python API library",
